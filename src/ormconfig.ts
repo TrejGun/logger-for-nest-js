a39ba5d90c19eee7c288343291fe4b69566c5d14
TrejGun <trejgun@gmail.com>
2022-05-07 07:47:02 +0800

    test: update jest

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,14 @@
 import "./env";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
-import { ConnectionOptions } from "typeorm";
-import path from "path";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
 import { UserEntity } from "./user/user.entity";
+import { AddTsdSchema1561991006215 } from "./migrations/1561991006215-add-schema";
+import { AddUserTable1562222612033 } from "./migrations/1562222612033-add-user-table";
+import { SetupUser1563804021014 } from "./migrations/1563804021014-setup-user";
 
 // Check typeORM documentation for more information.
-const config: ConnectionOptions = {
+const config: PostgresConnectionOptions = {
   name: "default",
   type: "postgres",
   url: process.env.POSTGRES_URL,
@@ -21,12 +23,7 @@ const config: ConnectionOptions = {
   // Allow both start:prod and start:dev to use migrations
   // __dirname is either dist or src folder, meaning either
   // the compiled js in prod or the ts in dev.
-  migrations: [path.join(__dirname, "/migrations/**/*{.ts,.js}")],
-  cli: {
-    // Location of migration should be inside src folder
-    // to be compiled into dist/ folder.
-    migrationsDir: "src/migrations",
-  },
+  migrations: [AddTsdSchema1561991006215, AddUserTable1562222612033, SetupUser1563804021014],
 };
 
 export default config;
